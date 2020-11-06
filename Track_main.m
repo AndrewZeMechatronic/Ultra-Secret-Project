79a93c285d7e427fca5c231f36b7d6790f9a7375
AndrewZeMechatronic <73714356+AndrewZeMechatronic@users.noreply.github.com>
2020-11-06 19:12:34 +0100

    Track generation code

---
+++ Track_main.m
@@ -0,0 +1,17 @@
+clc, clear
+
+n = 15; %number of points
+
+[P,x,y] = Track_function(n);
+
+
+scatter(x,y,"magenta")
+
+%Path plotting---------
+hold on
+
+plot(P(:,1),P(:,2), "-o", "color", "#ff9d12")
+%plot(x(k),y(k), "-")
+
+hold off
+%Path plotting--------

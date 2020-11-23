dc9b7a1f796953075acbec1a5806ca93012fc2e0
AndrewZeMechatronic <73714356+AndrewZeMechatronic@users.noreply.github.com>
2020-11-23 04:13:54 +0100

    We bots update
    Code ready car model.

--- controllers/ProjectXControler/ProjectXControler.m
+++ controllers/ProjectXControler/ProjectXControler.m
@@ -5,22 +5,40 @@
 % Author:
 % Modifications:
 
+%left_motor = wb_robot_get_device('left wheel motor');
+%right_motor = wb_robot_get_device('right wheel motor');
+%wb_motor_set_position(left_motor, inf);
+%wb_motor_set_position(right_motor, inf);
+%wb_motor_set_velocity(left_motor, 0);
+%wb_motor_set_velocity(right_motor, 0);
+
 % uncomment the next two lines if you want to use
 % MATLAB's desktop to interact with the controller:
-% desktop;
-% keyboard;
+desktop;
+%keyboard;
 
 TIME_STEP = 64;
 
 % get and enable devices, e.g.:
 %  camera = wb_robot_get_device('camera');
 %  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
+motor = wb_robot_get_device('rotational motor1');
+wb_motor_set_position(motor, inf);
+wb_motor_set_velocity(motor, 0);
+
+motor2 = wb_robot_get_device('rotational motor2');
+wb_motor_set_position(motor2, inf);
+wb_motor_set_velocity(motor2, 0);
+
+motors = wb_robot_get_device('rotational motor');
+%wb_motor_set_position(motors, inf);
+wb_motor_set_velocity(motors, 0);
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
+
 while wb_robot_step(TIME_STEP) ~= -1
 
   % read the sensors, e.g.:
@@ -29,7 +47,9 @@ while wb_robot_step(TIME_STEP) ~= -1
   % Process here sensor data, images, etc.
 
   % send actuator commands, e.g.:
-  wb_motor_set_velocity(rotational motor, 1)
+  wb_motor_set_velocity(motor, 0);
+  wb_motor_set_velocity(motor2, 0);
+  wb_motor_set_position(motors, 1);
   % if your code plots some graphics, it needs to flushed like this:
   drawnow;
 
